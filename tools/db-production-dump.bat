a0205cbaaa00a8a4e9105649bb5d06e849fdab2b
Alon <alon@empeeric.com>
2012-12-18 18:52:32 +0200

    adding cloudinary and mongolab addons to heroku tools/db-production-dump.bat tools/run.bat

--- tools/db-production-dump.bat
+++ tools/db-production-dump.bat
@@ -1,3 +1,3 @@
 cd ..
-mongodump -u user -p pass -h url --port 39427 -d user
+mongodump -u heroku_app10154408 -p jplikbbbd18odjtt96j3edek0k -h 10154408:@ds045757.mongolab.com --port 45757 -d heroku_app10154408
 pause
--- tools/run.bat
+++ tools/run.bat
@@ -1,9 +1,10 @@
 cd ..
 
+REM set MONGOLAB_URI=mongodb://heroku_app10154408:jplikbbbd18odjtt96j3edek0k@ds045757.mongolab.com:45757/heroku_app10154408
 set MONGOLAB_URI=mongodb://localhost/yt-elec-2013
 set PORT=80
 set ADMIN_PASSWORD=admin
-set CLOUDINARY_URL=
+set CLOUDINARY_URL=cloudinary://369171911744957:OfBLFSLXKj7jdWtmKlo2CH7I60o@hkf5hrwt
 
 node --debug app
 pause
