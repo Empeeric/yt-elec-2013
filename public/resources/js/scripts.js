c6aacb8156c32328918b4f5ce18a7835c877d7fe
Maria Tolstov <themarianne@gmail.com>
2012-12-30 19:57:52 +0200

    timer countdown

--- public/resources/js/scripts.js
+++ public/resources/js/scripts.js
@@ -1,6 +1,17 @@
 (function($){
     $(function(){
-        //your code goes here
+        $.countdown.regional['he'] = {
+            labels: ['שנים', 'חודשים', 'שבועות', 'ימים', 'שעות', 'דקות', 'שניות'],
+            labels1: ['שנה', 'חודש', 'שבוע', 'יום', 'שעה', 'דקה', 'שנייה'],
+            compactLabels: ['שנ', 'ח', 'שב', 'י'],
+            whichLabels: null,
+            digits: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+            timeSeparator: ':', isRTL: true};
+        $.countdown.setDefaults($.countdown.regional['he']);
+        var electionDate = new Date();
+        debugger;
+        electionDate = new Date(2013, 1, 22, 22, 0, 0, 0);
+        $('#timer').countdown({until: electionDate, digitHeight: 50/*, layout: '{sn} {sl} {mn} {ml} : {hn} {hl} : {dn} {dl}'*/});
     });
 }
 )(jQuery);
