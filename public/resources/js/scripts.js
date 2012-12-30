234d6896b2295f44232a2db22b5749aa2fb7ba34
Alon <alon@empeeric.com>
2012-12-30 21:13:36 +0200

    timer is on :)

--- public/resources/js/scripts.js
+++ public/resources/js/scripts.js
@@ -1,36 +1,30 @@
 (function($){
-        $.countdown.regional['he'] = {
-            labels:['שנים', 'חודשים', 'שבועות', 'ימים', 'שעות', 'דקות', 'שניות'],
-            labels1:['שנה', 'חודש', 'שבוע', 'יום', 'שעה', 'דקה', 'שנייה'],
-            compactLabels:['שנ', 'ח', 'שב', 'י'],
-            whichLabels:null,
-            digits:['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-            timeSeparator:':', isRTL:true};
-        $.countdown.setDefaults($.countdown.regional['he']);
-        var electionDate = new Date();
-        debugger;
-        electionDate = new Date(2013, 1, 22, 22, 0, 0, 0);
+    var electionDate = new Date(2013, 1, 22, 22, 0, 0, 0);
+
+    $(function(){
+        //init the timer
         $('#timer').countdown({until:electionDate,
             layout: '<div class="month">' +
                 '<div class="digit">{dn}</div>' +
                 '<br>{dl}' +
                 '</div>' +
                 '<div>' +
-                '<div class="digit">{hn}</div>' +
+                '<div class="digit">{hnn}</div>' +
                 '<br>{hl}' +
                 '</div>' +
                 '<div class="divider">:</div>' +
                 '<div>' +
-                '<div class="digit">{mn}</div>' +
+                '<div class="digit">{mnn}</div>' +
                 '<br>{ml}' +
                 '</div>' +
                 '<div class="divider">:</div>' +
                 '<div class="seconds">' +
-                '<div class="digit">{sn}</div>' +
+                '<div class="digit">{snn}</div>' +
                 '<br>{sl}' +
                 '</div>'
         });
-    })(jQuery);
+    })
+})(jQuery);
 
 
 
