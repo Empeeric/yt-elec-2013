847ab80de9f1dfbb57ab9fa63172159fe003982b
Maria Tolstov <themarianne@gmail.com>
2012-12-31 18:23:14 +0200

    google map

--- public/resources/js/scripts.js
+++ public/resources/js/scripts.js
@@ -24,6 +24,23 @@
                 '</div>'
         });
 
+        //init the map
+        var myLatLng = new google.maps.LatLng(31.7833, 35.2167);
+
+        var mapOptions = {
+            center: myLatLng,
+            zoom: 8,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map"),
+            mapOptions);
+
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title:"Hello World!"
+        });
+
         var $sub_menu = $('.menu ul ul li'),
             top_zindex = $sub_menu.length;
 
