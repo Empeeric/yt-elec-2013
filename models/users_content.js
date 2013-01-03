1de91dfcb097e160766889391411c825c8e17e57
Alon Valadji <alon@ronin.co.il>
2013-01-03 23:44:26 +0200

    users content in admin

---
+++ models/users_content.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    ObjectId = Schema.Types.ObjectId;
+
+var users_contentSchema = new Schema({
+    title: { type: String, required: true },
+    uploader: { type: String, required: true },
+    picture: { type: Schema.Types.Picture },
+    youtube_video_id: { type: String },
+    order: { type: Number, editable: false },
+    show: { type: Boolean, 'default': true }
+});
+
+var users_content = module.exports = mongoose.model('users_content', users_contentSchema);
