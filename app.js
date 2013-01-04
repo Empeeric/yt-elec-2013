67486c130058cd11594d51ac168b080ab29a7345
Refael Ackermann <refael@empeeric.com>
2013-01-04 12:34:47 +0200

    Some more logging

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 var express = require('express'),
     http = require('http'),
     path = require('path');
-
+var logger = require('./logger');
 // add some sugar baby
 require('sugar');
 
@@ -21,7 +21,7 @@ app.configure(function(){
     app.engine('html', require('consolidate').dust);
     app.set('view engine', 'html');
     app.set('views', path.join(__dirname, 'views'));
-
+    app.use(express.logger('dev'));
     app.use(express.compress());
     app.use(express.bodyParser());
     app.use(express.methodOverride());
@@ -29,6 +29,7 @@ app.configure(function(){
     app.use(express.cookieSession({cookie: { maxAge: (20).minutes() }}));
 
     app.use(express.static(path.join(__dirname, 'public')));
+    app.use(logger.logger_middleware);
 
     require('./admin')(app);
 
@@ -38,10 +39,13 @@ app.configure(function(){
         res.status(404);
         res.render('404', {title: 'The page cannot be found', content: '<p>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p><hr><p>Please try the following:</p><ul><li>Make sure that the Web site address displayed in the address bar of your browser is spelled and formatted correctly.</li><li>If you reached this page by clicking a link, contact the Web site administrator to alert them that the link is incorrectly formatted.</li><li>Click the <a href="javascript:history.back(1)">Back</a> button to try another link.</li></ul><h2>HTTP Error 404 - File or directory not found.</h2><hr>'});
     })
+    app.use(logger.catch_all_middleware);
+    app.use(logger.domain_wrapper_middleware)
 });
 
+logger.register_process_catcher()
+
 app.configure('development', function(){
-    app.use(express.logger('dev'));
     app.use(express.errorHandler());
 });
 
