d6bd89b53fa219813ecdb82b1361d16f0ba33d6a
Alon Valadji <alon@ronin.co.il>
2013-01-10 00:48:06 +0200

    adding grunt task to minify and bundle scripts and css

---
+++ grunt.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        min: {
+            dist: {
+                src: [
+                    'public/resources/js/json2.js',
+                    'public/resources/js/jquery.restjson.js',
+                    'public/resources/js/jquery.roundabout.js',
+                    'public/resources/js/jquery.roundabout-shapes.js',
+                    'public/resources/js/jquery.countdown.js',
+                    'public/resources/js/jquery.countdown-he.js',
+                    'public/resources/js/dust-full-1.1.1.js',
+                    'public/resources/js/dust-helpers-1.1.0.js',
+                    'public/resources/js/jquery.cloudinary.js',
+                    'public/resources/js/compiled_templates.js',
+                    'public/resources/js/scripts.js'
+                ],
+                dest: 'public/resources/js/build.min.js'
+            }
+        },
+
+        mincss: {
+            compress: {
+                files: {
+                    "public/resources/css/build.min.css": [
+                        'public/resources/css/style.css'
+                    ]
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-mincss');
+
+    grunt.registerTask('default', 'min mincss');
+};
